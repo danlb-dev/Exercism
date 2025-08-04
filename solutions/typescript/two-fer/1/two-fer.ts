20313a78e553a1884d833e10fe59d3e3b0c346c9
Daniel LB <danlb48986@gmail.com>
2025-08-04 14:30:14 -0600

    Update two-fer.ts

--- solutions/typescript/two-fer/1/two-fer.ts
+++ solutions/typescript/two-fer/1/two-fer.ts
@@ -1,7 +1,3 @@
-/**
- * This stub is provided to make it straightforward to get started.
- */
-
 export function twoFer(name:string): string {
   const person: string = name ? name : "you";
   return `One for ${person}, one for me.`;
